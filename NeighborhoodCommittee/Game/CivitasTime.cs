4633e6e1f01fe270d02c1753f3a0b6279949a385
Ji You <nightghostjiyou@gmail.com>
2014-08-15 13:07:28 +0800

    Add Class CivitasTime

---
+++ NeighborhoodCommittee/Game/CivitasTime.cs
@@ -0,0 +1,45 @@
+﻿namespace MCCCivitasBlackTech.Game
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class CivitasTime
+    {
+        public CivitasTime(DateTime relTime)
+        {
+            this.RelTime = relTime;
+        }
+
+        public CivitasTime(int day, int hour, int minute)
+        {
+            this.RelTime = new DateTime(2014, 8, 15, hour, minute, 0).AddDays(day - 515);
+        }
+
+        public DateTime RelTime
+        {
+            get;
+            private set;
+        }       
+
+        public int Day
+        {
+            get
+            {
+                TimeSpan timespan = this.RelTime - new DateTime(2014, 8, 15, this.Hour, this.Minute, 0);
+                return timespan.Days + 515;
+            }
+        }
+
+        public int Hour
+        {
+            get { this.RelTime.Hour; }
+        }
+
+        public int Minute
+        {
+            get { this.RelTime.Minute; }
+        }
+    }
+}
